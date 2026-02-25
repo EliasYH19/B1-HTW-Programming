49f04af10bbb87b69d9c4b0573146e7419955f30
EliasYH19 <eliasdiercks@gmail.com>
2026-02-25 17:56:50 +0100

    Week 3 Exercise 1 Solution

---
+++ Week3Exercise1.py
@@ -0,0 +1,18 @@
+score = int(input("Enter your score (0-100): "))
+
+if score >= 90:
+    grade = "A"
+elif score >= 80:  
+    grade = "B"
+elif score >= 70:  
+    grade = "C"
+elif score >= 60:  
+    grade = "D"
+else:
+    grade = "F"
+
+print(f"Your grade is: {grade}") # Added space in string
+
+# Bonus: print encouragement for top band grades
+if grade == "A":
+    print("Outstanding performance")
