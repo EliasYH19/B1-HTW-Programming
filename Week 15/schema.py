6b9ad4f556f5e1c294714fa82d459d2b9030f411
EliasYH19 <eliasdiercks@gmail.com>
2026-02-25 21:13:47 +0100

    Reformat schema.py

--- Week 15/schema.py
+++ Week 15/schema.py
@@ -1,12 +1,13 @@
 from pydantic import BaseModel
 
-# Model for creating a new user [cite: 60]
+# Model for creating a new user 
 class UserCreate(BaseModel):
     name: str
     email: str
 
-# Model representing a user with an ID [cite: 60]
+# Model representing a user with an ID
 class User(BaseModel):
     id: int
     name: str
+
     email: str
